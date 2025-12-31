97231b8fe82d48c33c33d1200a898d6a4077f40d
dhruvpateltwinnet <153700820+dhruvpateltwinnet@users.noreply.github.com>
2025-12-31 10:48:58 +0530

    Update CommonButton.jsx
    add space in CommonButton

--- src/components/CommonButton.jsx
+++ src/components/CommonButton.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const CommonButton = ({ name , onClick }) => {
     return (
+        
         <button onClick={onClick} style={{ background: "linear-gradient(91deg, #A01DBA -2.11%, #74ACF9 152.78%)" }} className='flex items-center justify-center py-2.5 rounded-xl w-full'>
             <div className='text-white text-lg leading-6 font-bold text-center'>
                 {name}
